f90cac6509cebc7c845fca023d1c2774e2f42f4d
Lch19970123 <508499598@qq.com>
2019-11-13 13:33:03 +0800

    提交注释

--- garage/src/com/huazi/garage/dao/DBinfo.java
+++
@@ -1,9 +0,0 @@
-package com.huazi.garage.dao;
-
-public interface DBinfo {
-	
-	public String DRIVER = "com.mysql.jdbc.Driver";
-	public String URL = "jdbc:mysql://localhost:3306/shop?characterEncoding=UTF-8";
-	public String USERNAME = "root";
-	public String PASSWORD = "123456";
-}
